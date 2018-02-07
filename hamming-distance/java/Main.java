8c93e75d302ee65d277631f93e6ef3e82dcdf332
cznno <smeraldo.ash@gmail.com>
2018-02-07 13:02:25 +0800

    update summary

--- hamming-distance/java/Main.java
+++ hamming-distance/java/Main.java
@@ -7,6 +7,9 @@ import java.util.function.BiFunction;
 public class Main {
 
     private static final String SEPARATOR = "============================================";
+    private int bestMethod = 1;
+    private int method = 1;
+    private long bestMethodCost = Integer.MAX_VALUE;
 
     public static void main(String[] args) {
         String a = args[0];
@@ -15,17 +18,29 @@ public class Main {
         System.out.println("String 2: " + b);
         System.out.println(SEPARATOR);
 
-        doCalc(a, b, Hamming::method1);
-        doCalc(a, b, Hamming::method2);
+        Main main = new Main();
+        main.doCalc(a, b, Hamming::method1);
+        main.method++;
+        main.doCalc(a, b, Hamming::method2);
+
+        System.out.println("Best method is: " + main.bestMethod);
+        System.out.println("Time cost is: " + main.bestMethodCost / 1000D + "us");
     }
 
-    private static void doCalc(String a, String b, BiFunction<String, String, Integer> hamming) {
+    private void doCalc(String a, String b, BiFunction<String, String, Integer> hamming) {
+
         long startTime = System.nanoTime();
         int res = hamming.apply(a, b);
         long endTime = System.nanoTime();
+
         long totalTime = endTime - startTime;
         System.out.println(res);
         System.out.println(totalTime / 1000D + "us");
         System.out.println(SEPARATOR);
+
+        if (totalTime < bestMethodCost) {
+            bestMethod = method;
+            bestMethodCost = totalTime;
+        }
     }
 }
